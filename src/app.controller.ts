15bd62d8e6eba557c259f5d1277b11c82033d141
ismaeil-shajar <msherif.ali6@gmail.com>
2022-03-06 21:37:32 +0200

    get host from node name

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,22 +3,24 @@ import { AppService } from './app.service';
 import { Catn } from './cat.schema';
 import { CreateCatDto } from './create-cat.dto';
 import * as os from 'os';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly configService: ConfigService,
+    private readonly appService: AppService) {}
 
   @Get()
   async getHello(){
    let out= await this.appService.findOne(1);
-    return {"hits":out.hits,"hostName":os.hostname(),"success":true};
+    return {"hits":out.hits,"hostName": this.configService.get("MY_NODE_NAME")||os.hostname(),"success":true};
   }
   @Get("rest")
   async getReset(){
    let out= await this.appService.reset();
     return "done";
   }
-  
+
   @Get("liveness")
   async getLive(){
     return "Done";
